2bcdf183b47b9df89e881e78dc28d2cabe28aed5
deboracaires <deboracaires@pop-os.localdomain>
2022-03-07 15:40:13 -0300

    preparando .env

--- src/connection/database.js
+++ src/connection/database.js
@@ -3,11 +3,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: 'bootcamp_role',
-  host: 'localhost',
-  port: 5432, 
-  database: 'boardcamp',
-  password: 'senha_super_hiper_ultra_secreta_do_role_do_bootcamp'
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default connection;
