b881faaecb109f3524aef61e1f1345f61d59e8a5
deboracaires <deboracaires@pop-os.localdomain>
2022-03-07 14:47:35 -0300

    rota post/rentals/:id/return finalizada

--- src/controllers/rentalController.js
+++ src/controllers/rentalController.js
@@ -9,7 +9,7 @@ export async function postRental (req, res) {
       daysRented,
     } = req.body;
 
-    const rentDate = dayjs().format('MM/DD/YYYY');
+    const rentDate = dayjs().format('DD/MM/YYYY');
     
     const originalPrice = Number(res.locals.gameData.pricePerDay) * daysRented;
 
@@ -83,6 +83,50 @@ export async function getRentals (req, res) {
 
 
     res.send(rental)
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+export async function finishRental (req, res) {
+  try {
+    const id = req.params.id;
+
+    const verifyId = await connection.query(`
+      SELECT * FROM rentals WHERE id = $1
+    `, [id]);
+
+    if (verifyId.rowCount === 0) return res.sendStatus(404);
+
+    if (verifyId.rows[0].returnDate !== null) return res.sendStatus(400);
+
+    const returnDate = dayjs().format('YYYY-MM-DD');
+    const daysRented = verifyId.rows[0].daysRented;
+    const rentDate = dayjs(verifyId.rows[0].rentDate).format('YYYY-MM-DD');
+    
+    const verifyDate = dayjs(rentDate).add(daysRented, 'day').format('YYYY-MM-DD');
+    
+    const finalDate = new Date(returnDate);
+    const verifyFinalDate = new Date(verifyDate);
+
+    const differenceInDays = (Math.abs(finalDate - verifyFinalDate)/(1000*60*60*24));
+
+    let delayFee = 0;
+
+    if(verifyDate < Math.abs(finalDate - verifyFinalDate)){
+            
+      delayFee = differenceInDays * (verifyId.rows[0].originalPrice / daysRented);
+
+    }else{
+      delayFee = null;
+    }
+
+    await connection.query(`
+      UPDATE rentals SET "returnDate" = $2, "delayFee" = $3 WHERE id = $1
+    `, [id, returnDate, delayFee]);
+        
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
--- src/routes/rentalRouter.js
+++ src/routes/rentalRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getRentals, postRental } from '../controllers/rentalController.js';
+import { finishRental, getRentals, postRental } from '../controllers/rentalController.js';
 import { validateNewRental } from '../middlewares/rentalMiddlewares.js';
 
 const rentalRouter = Router();
 
 rentalRouter.get('/rentals', getRentals);
 rentalRouter.post('/rentals', validateNewRental,  postRental);
+rentalRouter.post('/rentals/:id/return', finishRental);
 
 export default rentalRouter;
