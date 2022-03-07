c15f0b377fead120f5d08d99e72e7822432da315
deboracaires <deboracaires@pop-os.localdomain>
2022-03-06 21:10:47 -0300

    rota get/games finalizada

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -1,5 +1,35 @@
 import connection from '../connection/database.js';
 
+export async function getGames (req, res) {
+  try {
+    let name = '';
+
+    if (req.query.name) name = `WHERE games.name LIKE '${req.query.name}%'`;
+
+
+    const games = await connection.query({
+        text: `
+          SELECT games.*,
+          categories.name as "categoryName" FROM categories
+          JOIN games on games."categoryId" = categories.id
+          ${name}
+        `,
+        rowMode: 'array'
+    });
+    res.send(games.rows.map(row => {
+      const [id, name, image, stockTotal, categoryId, pricePerDay, categoryName] = row;
+
+        return {
+          id, name, image, stockTotal, categoryId, pricePerDay, categoryName
+        }
+    }));
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+
+
 export async function postGame (req, res) {
   try {
     const {
--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { postGame } from '../controllers/gameController.js';
+import { getGames, postGame } from '../controllers/gameController.js';
 import { validateNewGame } from '../middlewares/gameMiddleware.js';
 
 const gameRouter = Router();
 
+gameRouter.get('/games', getGames);
 gameRouter.post('/games', validateNewGame, postGame);
 
 export default gameRouter;
